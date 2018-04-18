36afda785cdbd3824e909e7547efb965fc5db81c
sdygt <root@sdygt.net>
2018-04-18 23:33:00 +0800

    Fix placeholder

--- controllers/uav.js
+++ controllers/uav.js
@@ -3,32 +3,15 @@ const app = express();
 
 const mUAV = require('../models/uav');
 
-app.get('/', function (req, res) {
-    mUAV.getAll()
-        .then(data => {
-            res.status(200).json(data).end();
-        })
-        .catch(err => {
-            res.status(500).end(err.message);
-        });
+app.get('/', function (req, res, next) {
+    next(new Error('Not yet implemented\nGET /uav'));
 });
 
-app.get('/:id', (req, res) => {
-    mUAV.getOne(req.params.id)
-        .then(data => {
-            if (data) {
-                res.status(200).json(data).end();
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch(err => {
-            console.warn(err.stack);
-            res.status(500).end(err.message);
-        });
+app.get('/:id', (req, res, next) => {
+    next(new Error('Not yet implemented\nGET /uav/:id'));
 });
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
     let iUAV = mUAV.getNewInstance(req.body);
 
     mUAV.add(iUAV)
@@ -41,34 +24,12 @@ app.post('/', (req, res) => {
         });
 });
 
-app.put('/:id', (req, res) => {
-    if (!req.body.text) {
-        res.status(400).end('Field `text` not set');
-        return;
-    }
-    mUAV.update(req.params.id, req.body.text)
-        .then(r => {
-            if (r.modifiedCount === 1) {
-                res.status(200).end();
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch(err => {
-            console.warn(err.stack);
-            res.status(500).end(err.message);
-        });
+app.put('/:id', (req, res, next) => {
+    next(new Error('Not yet implemented\nPUT /uav/:id'));
 });
 
-app.delete('/:id', (req, res) => {
-    mUAV.remove(req.params.id)
-        .then(r => {
-            res.status(204).end();
-        })
-        .catch(err => {
-            console.warn(err.stack);
-            res.status(500).end(err.message);
-        });
+app.delete('/:id', (req, res, next) => {
+    next(new Error('Not yet implemented\nDELETE /uav/:id'));
 });
 
 
